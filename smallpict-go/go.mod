4a8fb043ad9c27bf9d6d1586e380b639da10dbb0
4IP <4ipxxx@gmail.com>
2026-09-01 23:49:56 +0700

    fix conflict
    Signed-off-by: 4IP <4ipxxx@gmail.com>

--- smallpict-go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/tuxnoob/smallpict-go
-
-go 1.21
--- smallpict-java/src/main/java/com/smallpict/errors/ServerException.java
+++
@@ -1,9 +0,0 @@
-package com.smallpict.errors;
-
-import java.util.Map;
-
-public class ServerException extends SmallPictException {
-    public ServerException(String message, int statusCode, String requestId, Map<String, Object> details) {
-        super(message, "INTERNAL_ERROR", statusCode, requestId, details, null);
-    }
-}
